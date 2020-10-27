f91dea1da7ba6d75fb9bf06e8893f786787aad91
Dong Huu Cao <43071789+dcaohuu18@users.noreply.github.com>
2020-10-26 23:28:44 -0400

    First Commit

---
+++ Assets/Scripts/TestBacteria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBacteria : MonoBehaviour
+{
+	public float bacteriaSpeed = 2;
+	private float new_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        new_y = Random.Range(-2f, 2f);
+        transform.Translate(new Vector3(bacteriaSpeed*Time.deltaTime, new_y*Time.deltaTime, 0));
+    }
+}
